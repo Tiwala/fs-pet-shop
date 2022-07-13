a89a3e41d56f50e7d20f3fbced02ea806dbe5027
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-07-13 17:39:16 -0500

    finished create

--- pets.js
+++ pets.js
@@ -1,42 +1,60 @@
 import fs from 'fs';
 
 const subcommand = process.argv[2];
-const indexNumber = process.argv[3];
+
 
 
 
 
 fs.readFile('pets.JSON', 'utf-8', (err, str) => {
     const data = JSON.parse(str);
-    // for (var i = 0; i < data.length; i++) {
-    //     if (subcommand === `read ${i}`) {
-    //         console.log(data[i]);
-    //     }
-    // }
-    // for (var i = 0; i < data.length; i++) {
-    //     switch (indexNumber) {
-    //         case i.toString(): {
-    //             console.log(data[i]);
-    //         }
-    //     }
-    // }
     switch(subcommand) {
         case 'read': {
-            if (indexNumber != undefined) {
-                for (var i = 0; i < data.length; i++) {
-                    if (indexNumber === i.toString()) {
-                        console.log(data[i], 'data');
-                    }
-                }
-            } else {
-                console.log(data, 'data')
-            }
+            read(data);
+            break;
         }
-        case 'create':
+        case 'create': {
+            create(data);
+            break;
+        }
         case 'update':
         case 'destroy':
         default: {
             console.error('Usage: node pets.js [read | create | update | destroy]');
+            //exits
+            process.exit(1);
         }
     }
 })
+
+function read(data) {
+    let indexNumber = process.argv[3];
+        if (indexNumber != undefined) {
+            if (data[indexNumber] != undefined) {
+                console.log(data[indexNumber])
+            } else {
+                console.log('Usage: node pets.js read INDEX')
+            }
+        } else {
+        console.log(data, 'data')
+    }
+}
+
+function create(data) {
+    let petAge = process.argv[3];
+    let petKind = process.argv[4];
+    let petName = process.argv[5];
+    let newPet = {
+        'age': Number(`${petAge}`),
+        'kind': petKind,
+        'name': petName
+    }
+    data.push(newPet);
+    fs.writeFile('pets.json', JSON.stringify(data), (err) => {
+        if (err) {
+            console.error('error');
+        } else {
+            console.log(newPet)
+        }
+    })
+}
